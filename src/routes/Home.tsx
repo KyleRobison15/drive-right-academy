b2f46202c93bf88cfce8807355dc5b6766d5abea
KyleRobison15 <kylerobison15@gmail.com>
2024-03-17 16:59:20 -0600

    Intial home page design

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,31 @@
+import { Grid, GridItem, HStack } from "@chakra-ui/react";
+import homeImage from "../assets/mini.jpg";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Grid
+      templateAreas={`"top"
+                      "bottom"`}
+      gridTemplateRows={"50% 50%"}
+      gridTemplateColumns={"1fr"}
+      w="100%"
+    >
+      <GridItem area={"top"}>
+        <HStack
+          h="100%"
+          w="100%"
+          bgImage={homeImage}
+          bgSize="cover"
+          bgPos="center"
+          bgRepeat="no-repeat"
+          opacity="0.3"
+        ></HStack>
+      </GridItem>
+      <GridItem p="20px" bg="gray.100" area={"bottom"}>
+        Bottom
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default Home;
