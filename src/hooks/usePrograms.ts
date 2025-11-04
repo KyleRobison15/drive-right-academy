65f1c2ab18a538be109f5c5640622d18512e7c78
KyleRobison15 <kylerobison15@gmail.com>
2025-11-04 10:05:49 -0700

    Remove teen driving program, and update a la carte in Programs page

--- src/hooks/usePrograms.ts
+++ src/hooks/usePrograms.ts
@@ -1,7 +1,7 @@
 import { IconType } from "react-icons";
-import { BiSolidTrafficCone } from "react-icons/bi";
 import { FaIdCard } from "react-icons/fa6";
 import { TbManualGearbox } from "react-icons/tb";
+import { FaClipboardList } from "react-icons/fa";
 
 export interface CardLink {
   label: string;
@@ -42,17 +42,17 @@ export interface AlaCarte {
 
 const alaCarte: AlaCarte[] = [
   {
-    name: "skills-lab",
-    label: "8-Hour Skills Based Driving Lab",
+    name: "permit-test",
+    label: "Permit Test",
     description: "8-Hour, hands-on driving class with Drive-Right instructors.",
-    price: "$149",
-    icon: BiSolidTrafficCone,
+    price: "$45",
+    icon: FaClipboardList,
     aria: "Traffic Cone Icon",
   },
   {
     name: "driving-test",
     label: "Driving Test",
-    price: "$75",
+    price: "$70",
     description: "",
     icon: FaIdCard,
     aria: "Identification Card Icon",
@@ -70,57 +70,8 @@ const alaCarte: AlaCarte[] = [
 
 const programs: Program[] = [
   {
-    name: "teen-driving-program",
-    label: "Teen Driving Program",
-    price: "$479",
-    image: "",
-    description:
-      "The 40-Hour Teen Program is a comprehensive Driver's Ed program that meets ALL the state requirements for students 14 years old and 11 months to obtain their learner's permit. It then meets ALL the state requirements of the 6 Hours Behind the Wheel for students to obtain their license. Also, most insurance companies require this program to receive any type of discounts on insurance premiums.",
-    highlights: [
-      {
-        text: "Our most popular program",
-        badgeText: null,
-      },
-      {
-        text: "Three 8-hour classroom days",
-        badgeText: null,
-      },
-      {
-        text: "One day skills-based driving lab in Student's vehicle",
-        badgeText: null,
-      },
-      {
-        text: "Two 3-hour Behind the Wheel appointments in a Drive-Right Academy vehicle",
-        badgeText: null,
-      },
-      {
-        text: "The Colorado written permit test",
-        badgeText: null,
-      },
-    ],
-    links: null,
-    accordionItemDetails: [
-      {
-        label: "Enrollment Details",
-        highlights: [
-          "Initial enrollment can be handled over the phone.",
-          "Official Registration and Payment are handled the first day of class.",
-          "Depending on availability, cars for the Driving Lab may be rented for an additional $50 if the student is unable to provide one.",
-        ],
-        text: null,
-      },
-      {
-        label: "Manual Transmission Training",
-        highlights: null,
-        text: "Have a car with a manual transmission? For an additional $100/hr over our normal 40-Hour Teen Driving Program we'll teach you driving skills in a standard-shift car.",
-      },
-    ],
-    footerText:
-      "* A non-refundable, $50 deposit which counts toward your balance is required to enroll.",
-  },
-  {
     name: "behind-the-wheel",
-    label: "Behind the Wheel",
+    label: "6-Hour Behind the Wheel",
     price: "$390",
     image: "",
     description:
@@ -146,11 +97,11 @@ const programs: Program[] = [
   },
   {
     name: "adult-driving-program",
-    label: "Adult Driving Program",
+    label: "4-Hour Adult Driving Program",
     price: "$260",
     image: "",
     description:
-      "Please call for more information on our Adult Driving Program.",
+      "You must have a permit to enroll in this program. Please call for more information on our Adult Driving Program.",
     highlights: [
       {
         text: "Two 2-hour, skills-based, private road lessons",
@@ -177,10 +128,10 @@ const programs: Program[] = [
   {
     name: "online-drivers-education",
     label: "Online Driver's Education",
-    price: null,
+    price: "$74",
     image: "",
     description:
-      "Is your schedule too hectic for an in person class? The Colorado Teen On-line Program allows you to get the required 30 hour course completed on your own time. Pricing is available when you sign up and create your account.",
+      "The Colorado Teen On-line Program allows you to get the required 30 hour course completed on your own time.",
     highlights: [
       {
         text: "Online",
